2179cd8584811dd970a5a175d3d5caded56a7777
SachithKumar0628 <sachith.k@s.amity.edu>
2025-05-06 11:58:29 +0530

    Added Test1

--- marks.c
+++ marks.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    float test1, test2, total;
+    float test1_subject1;
 
-     printf("Enter Test 1 marks for Subject 1: ");
+    printf("Enter Test 1: ");
     scanf("%f", &test1);
 
-    printf("Enter Test 2 marks for Subject 2: ");
-    scanf("%f", &test2);
+    printf("Test 1 marks = %.2f\n", test1);
 
-    total = test1 + test2;
-    printf("Total marks = %.2f\n", total);
-
-    
     return 0;
 }
 
